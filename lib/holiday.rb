45e6663e1bf94aebc4598fc8504b005ab222499c
DJ Taylor <dtaylor88@gmail.com>
2021-06-02 02:20:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-      holiday.values
+      puts "#{season}:"
+      puts "#{holiday}: #{holiday.values"
+      
       binding.pry
   end
 end
